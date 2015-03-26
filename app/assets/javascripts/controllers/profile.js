5a17499d95c5160c37252c2b8456b419d50d7ef7
GIT Admin <sis17@aber.ac.uk>
2015-03-26 09:56:36 +0000

    update password

--- app/assets/javascripts/controllers/profile.js
+++ app/assets/javascripts/controllers/profile.js
@@ -1,19 +1,34 @@
 hcApp.controller('ProfileCtrl', [
-  '$scope', '$location', 'Restangular', 'notice', 
+  '$scope', '$location', 'Restangular', 'notice',
   function($scope, $location, Restangular, notice) {
+    $scope.passwordNew = '';
+    $scope.passwordConf = '';
 
     $scope.saveProfile = function() {
-      profile = {
+      var profile = {
         email: $scope.$storage.user.email,
         name: $scope.$storage.user.name,
         colour: $scope.$storage.user.colour
       }
 
+      if ($scope.passwordNew.length > 0) {
+        profile.password = $scope.passwordNew;
+        if ($scope.passwordConf == $scope.passwordNew) {
+          $scope.updateProfile(profile);
+        } else{
+          notice('Passwords Don`t Match', 'In order to update your password, please provide a matching confirmation password.', 'danger', 6);
+        }
+      } else {
+        $scope.updateProfile(profile);
+      }
+    }
+
+    $scope.updateProfile = function(profile) {
       Restangular.one('users', $scope.$storage.user.id).patch({profile:profile}).then(function(response) {
-        notice(response.message.title, response.message.text, response.message.type, 2);
+        notice(response.message.title, response.message.text, response.message.type, 4);
       }, function() {
-        notice('Profile Not Saved', 'Sorry, but we can\'t save your profile right now.', 'warning', 2);
-      })
+        notice('Profile Not Saved', 'Sorry, but we can\'t save your profile right now.', 'warning', 4);
+      });
     }
   }
 ]);
